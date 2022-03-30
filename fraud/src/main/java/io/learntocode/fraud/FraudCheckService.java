3c2c90bb001e3809036fe4debc3fbf3aff8ffef7
RDNATH <im_rahuldebnath@outlook.com>
2022-03-30 12:15:24 +0530

    fraud detection service

---
+++ fraud/src/main/java/io/learntocode/fraud/FraudCheckService.java
@@ -0,0 +1,24 @@
+package io.learntocode.fraud;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FraudCheckService {
+	
+	private final FraudCheckHistoryRepository  fraudCheckHistoryRepository;
+	
+	public FraudCheckService(FraudCheckHistoryRepository  fraudCheckHistoryRepository) {
+		this.fraudCheckHistoryRepository=fraudCheckHistoryRepository;
+	}
+
+	public boolean isFradulentCustomer(Integer customerId) {
+		fraudCheckHistoryRepository.save(FraudCheckHistory.builder()
+														  .customerId(customerId)
+														  .isFraudster(false)
+														  .createdAt(LocalDateTime.now())
+														  .build());
+		return false;
+	}
+}
